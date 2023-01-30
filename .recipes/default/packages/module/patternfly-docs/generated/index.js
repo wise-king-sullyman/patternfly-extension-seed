58316a1baa4f0a73a825728c55cedb0ffb8b7397
Austin Sullivan <ausulliv@redhat.com>
2023-01-30 09:15:55 -0500

    chore(structure): reduce documentation directory nesting

--- .recipes/default/packages/module/patternfly-docs/generated/index.js
+++ .recipes/default/packages/module/patternfly-docs/generated/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  '/extensions/{{ tmplr.extension_name | kebab-case }}/design-guidelines': {
+  '/design-guidelines': {
     id: "{{ tmplr.extension_name }}",
     title: "{{ tmplr.extension_name }}",
     toc: [{"text":"Header"},[{"text":"Sub-header"}]],
     section: "extensions",
     source: "design-guidelines",
-    Component: () => import(/* webpackChunkName: "extensions/{{ tmplr.extension_name | kebab-case }}/design-guidelines/index" */ './extensions/{{ tmplr.extension_name | kebab-case }}/design-guidelines')
+    Component: () => import(/* webpackChunkName: "design-guidelines/index" */ './design-guidelines')
   },
-  '/extensions/{{ tmplr.extension_name | kebab-case }}/react': {
+  '/react': {
     id: "{{ tmplr.extension_name }}",
     title: "{{ tmplr.extension_name }}",
     toc: [{"text":"Basic usage"},[{"text":"Example"},{"text":"Fullscreen example"}]],
@@ -15,6 +15,6 @@ module.exports = {
     fullscreenExamples: ["Fullscreen example"],
     section: "extensions",
     source: "react",
-    Component: () => import(/* webpackChunkName: "extensions/{{ tmplr.extension_name | kebab-case }}/react/index" */ './extensions/{{ tmplr.extension_name | kebab-case }}/react')
+    Component: () => import(/* webpackChunkName: "react/index" */ './react')
   }
 };
